b8b5c6e0d8cb1d81b1c89ed35f31720a0c4079d0
youbl <youbl@126.com>
2021-01-22 23:48:45 +0800

    资产管理系统启动

---
+++ asset-management/src/test/java/beinet/cn/assetmanagement/AssetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package beinet.cn.assetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
