19a9588c1f6fce57792de76101f86ab319eefbca
youbl <youbl@126.com>
2021-05-28 16:42:18 +0800

    提醒时间工具：创建，并设置程序和窗体icon

---
+++ RemindClock/RemindClock/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using RemindClock.Properties;
+
+namespace RemindClock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            // 设置窗体icon
+            this.Icon = Resources.clock;
+        }
+    }
+}
