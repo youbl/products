d5d77b6c593affef1213aa10ba295218e96d7a59
youbl <youbl@126.com>
2021-05-28 16:51:18 +0800

    提醒时间工具：添加最小化到托盘的功能

--- RemindClock/RemindClock/MainForm.Designer.cs
+++ RemindClock/RemindClock/MainForm.Designer.cs
@@ -29,12 +29,31 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.MainForm_SizeChanged);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- RemindClock/RemindClock/MainForm.cs
+++ RemindClock/RemindClock/MainForm.cs
@@ -11,5 +11,37 @@ namespace RemindClock
             // 设置窗体icon
             this.Icon = Resources.clock;
         }
+
+        /// <summary>
+        /// 最小化到托盘的双击事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.ShowInTaskbar = true; // 任务栏显示
+            this.Visible = true; // 设置窗体可见
+            this.WindowState = FormWindowState.Normal; // 设置窗体为默认大小
+            this.Activate(); // 激活当前窗口，避免不可见
+            notifyIcon1.Visible = false;
+        }
+
+        /// <summary>
+        /// 窗体大小变更事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_SizeChanged(object sender, System.EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                // 进入最小化状态时, 任务栏隐藏
+                this.ShowInTaskbar = false;
+                // 托盘可见
+                notifyIcon1.Visible = true;
+                // 隐藏窗体
+                this.Visible = false;
+            }
+        }
     }
 }
