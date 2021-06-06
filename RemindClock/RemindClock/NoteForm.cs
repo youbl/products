98b4233811517c91db87736b70b68e5b64bb35ac
youbl <youbl@126.com>
2021-06-06 21:40:45 +0800

    提醒时间工具：编辑提醒的窗体初始化

--- RemindClock/RemindClock/NoteForm.cs
+++ RemindClock/RemindClock/NoteForm.cs
@@ -8,23 +8,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using RemindClock.Repository.Model;
+using RemindClock.Services;
 
 namespace RemindClock
 {
     public partial class NoteForm : Form
     {
-        private Notes notes;
+        private readonly Notes notes;
+        private readonly NotesService notesService = new NotesService();
 
         public NoteForm(Notes notes)
         {
             this.notes = notes ?? throw new ArgumentException("提醒不能为空");
             InitializeComponent();
+
+            this.txtTitle.Text = this.notes.Title;
+            this.txtContent.Text = this.notes.Content;
         }
 
         private void NoteForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             // this.Hide();
             // e.Cancel = true;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            notesService.Save(this.notes);
+            MessageBox.Show("保存成功");
         }
     }
 }
