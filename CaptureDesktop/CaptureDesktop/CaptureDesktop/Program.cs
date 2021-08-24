07d2f757ad6e411467c8d4871af474e5b4f2359e
youbl <youbl@126.com>
2021-08-24 10:13:26 +0800

    控制台-屏幕截图工具：截图功能实现

---
+++ CaptureDesktop/CaptureDesktop/CaptureDesktop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing.Imaging;
+
+namespace CaptureDesktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now + " 开始...");
+            CaptureUtil.Default.CaptureScreenToFile("D:\\a.jpg", ImageFormat.Jpeg);
+            Console.WriteLine(DateTime.Now + " 完成.");
+        }
+    }
+}
